50deeb0296a40185cd8ec399be1b75c72ec67ed1
Pavel Jurča <pavel.jurca@hotmail.com>
2016-05-20 22:13:22 +0200

    @todo

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ### To update czech interface of Greenstone2, missing translations, bad spelling and remove outdated macros
 
-..based on english originals `english_orig.md` and `english2_orig.md` or more up-to-date slovak ones.
+based on english originals `english_orig.md` and `english2_orig.md` or more up-to-date slovak ones.
+
+
+NAME
+    diff_macros.pl - compare two DM files
+
+USAGE
+    diff_macros.pl FILE1.dm FILE2.dm
+
+DESCRIPTION
+    Compare macro names of the two DM files, i.e. outdated macros to be removed and missing ones to be added.
+
--- diff_macros.pl
+++ diff_macros.pl
@@ -1,68 +1,82 @@
 use strict;
 use warnings;
 use open qw/:std :utf8/;
+use autodie;
 use utf8;
-#use autodie;
 
 # CHANGELOG
 # v0.1, 5/18/16, Pavel Jurca
-#   — print outdated macros to be removed and missing macros to be added
+#   — diff macro names
 
 # TODO
-# [will be discussed in the next meeting]
+# make it a more general use
+# [will be discussed in a next meeting]
 
 
 ### ============== MAIN =============
+unless (@ARGV == 2) {
+  #grep /--help|-h|\/\?/, @ARGV
+  print <<'EOL';
+NAME
+    diff_macros.pl - compare two DM files
 
-print check_macros(
-  \get_macros('english_orig.dm'), # ORIGINAL file
-  \get_macros('czech_orig.dm') # TRANSLATED file to check
+USAGE
+    diff_macros.pl FILE1.dm FILE2.dm
+
+DESCRIPTION
+    Compare macro names of the two DM files, i.e. outdated macros to be removed and missing ones to be added.
+
+EOL
+exit 2;
+}
+
+print diff_macro_names(
+  get_macro_names(shift), # FILE1.dm
+  get_macro_names(shift)  # FILE2.dm
 );
-
 ### ============ END ================
 
 
 
-=head2 get_macros
+=head2 get_macro_names
 
-Extract macros' names from the given DM file.
+Extract macro names from the given DM file.
 
- Return  : @ macros' names
+ Return  : % line_ref (of macro names)
  Args    : source file
 
 =cut
 
-sub get_macros {
-  @ARGV = shift;
-  map {
-    /^_([^_]+)/ and $1
-  } <>;
+sub get_macro_names {
+  my $file = shift;
+  die qq/(!) "$file" doesn't exist\n/
+    unless -e $file;
+
+  open my $in, '<:utf8', $file;
+  my %line_of = map {
+                #line number, macro name
+    /^_([^_]+)/ and ($., $1)
+  } <$in>;
+
+  return \%line_of;
 }
 
-=head2 check_macros
+=head2 diff_macro_names
 
+Tell which macro names differ between the two of files.
 
-
- Return  : printf text
- Args    : @ en_ref, cz_ref
+ Return  : sprintf text
+ Args    : % line_ref1, line_ref2 (of macro names)
 
 =cut
 
-sub check_macros {
+sub diff_macro_names {
+  return -1 unless @_ == 2;
 
 
-# $. => print line numbers!
+#sort keys 
 
-#my %en = map {
-#  ($_, undef)
-#} get_macros('english_orig.dm');
-#print "$_\n" for keys %en; exit;
-#
-#my @cz = get_macros('czech_orig.dm');
-#for my $macro (keys %en) {
+#return sprintf("%s",);
+
 #  $en{$macro} = grep /$macro/, @cz ? 1 : 0;
-#}
-#
-#  return printf
-
 }
